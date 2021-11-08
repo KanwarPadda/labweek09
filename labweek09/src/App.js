13be1ba272c41faa688c91d7a41cc85b67fd6aad
Kanwartejbir Singh Padda <90290779+KanwarPadda@users.noreply.github.com>
2021-11-07 19:36:40 -0500

    Add files via upload

---
+++ labweek09/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+         Welcome to Fullstack Development - I
+        </h1>
+        <h2>React JS Programming Week09 Lab exercise
+          </h2>
+          <h3>101280244</h3>
+          <p>Kanwartejbir Singh Padda</p>
+          <p>George Brown College, Toronto</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
